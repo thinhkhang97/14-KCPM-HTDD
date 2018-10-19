92cf1f5a0fe3eafc22702a4b1b393f30989640be
DESKTOP-FSI8QAQ\Nguyen Thinh Khang <thinhkhang97@gmail.com>
2018-10-19 22:53:48 +0700

    9 files change

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-    public apiHost = 'https://tpltesting.herokuapp.com/api';
-    public host = 'https://tpltesting.herokuapp.com';
+    public apiHost = 'https://kcpm-14-htdd.herokuapp.com/api';
+    public host = 'https://kcpm-14-htdd.herokuapp.com';
 }
